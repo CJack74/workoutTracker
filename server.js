9e2d5001262fddb978b886249bf33779b6a35ae3
CJ Dodge <hitmancjack@yahoo.com>
2020-12-01 16:00:41 -0800

    added npm express, mongoose, and morgan packages. Also made a server.js file.

---
+++ server.js
@@ -0,0 +1,13 @@
+//Dependencies
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
