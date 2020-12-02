137b5e35b313b0daa0a603324c23b85a4cf1bc6e
CJ Dodge <hitmancjack@yahoo.com>
2020-12-01 16:36:16 -0800

    created workout model in workout.js and started working on api-routes.js

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,57 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+      day: {
+        type: Date,
+        default: () => new Date()
+      },
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: "Exercise Type"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Name of Exercise"
+          },
+          duration: {
+            type: Number,
+            required: "Enter workout duration"
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true
+      }
+    }
+  );
+  
+  // function for getting total duration of exercises
+  workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
+  
+  const Workout = mongoose.model("Workout", workoutSchema);
+  
+  module.exports = Workout;
